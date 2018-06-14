447c0767c723e88cc4594d9c0f24c063a183e14c
stkay <stkayy@gmail.com>
2018-06-14 11:38:36 +0900

    heroku対応

--- src/server/app.ts
+++ src/server/app.ts
@@ -5,7 +5,7 @@ const app: express.Express = express();
 // app.use("/", routes);
 app.use(express.static('public'));
 
-const port = "3000";
+const port = process.env.PORT || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
