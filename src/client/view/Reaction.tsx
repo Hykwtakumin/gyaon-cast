da320b3899eb7789cad518f4b1026a1610191a6f
stkay <stkayy@gmail.com>
2018-07-02 14:58:17 +0900

    リアクションリストのUIを微調整

--- src/client/view/Reaction.tsx
+++ src/client/view/Reaction.tsx
@@ -9,22 +9,30 @@ interface ReactionProps {
 
 export default class Reaction extends React.Component<ReactionProps, {}> {
     private readonly wrapperStyle = {
+        display: "flex",
         marginBottom: 10
     };
 
-    render(){
+    render() {
         const {title, time, user, url, links} = this.props.reactionParams;
+        const timeEl = <div style={{flex: "1 1 0%"}}>{dateFormat(new Date(time), "mm/dd HH:MM")}</div>;
         const reactionAudio = <HoverToPlayText src={url} text={title}/>;
-        const reactionEl = <div style={{whiteSpace: "nowrap"}}>{dateFormat(new Date(time), "mm/dd HH:MM")} {user}「{reactionAudio}」</div>;
+        const reactionEl = <div style={{whiteSpace: "nowrap"}}>{user}「{reactionAudio}」</div>;
         const records = links.map((record: Gyaon) => {
             const {title, url, user} = record;
             const recordedAudio = <HoverToPlayText src={url} text={title}/>;
-            return <div style={{marginLeft: 20, whiteSpace: "nowrap"}}>{user} 「{recordedAudio}」</div>
+            return <div style={{whiteSpace: "nowrap", overflow: "hidden"}}>{user} 「{recordedAudio}」</div>
         });
-        return(
-            <div style={this.wrapperStyle}>
-                {reactionEl}
+        const recordAndReactionEl = (
+            <div style={{flex: "2.6 1 0%", overflow: "auto"}}>
                 {records}
+                {reactionEl}
+            </div>
+        );
+        return (
+            <div style={this.wrapperStyle}>
+                {timeEl}
+                {recordAndReactionEl}
             </div>
         )
     }
